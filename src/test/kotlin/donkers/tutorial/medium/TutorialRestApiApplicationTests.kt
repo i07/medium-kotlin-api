bd2b1b520d55a392587cb31e510ce181c8ae8d93
Marc Donkers <marc@i07.eu>
2022-05-08 23:02:31 +0200

    Unit tests tutorial on Medium

--- src/test/kotlin/donkers/tutorial/medium/TutorialRestApiApplicationTests.kt
+++ src/test/kotlin/donkers/tutorial/medium/TutorialRestApiApplicationTests.kt
@@ -1,13 +1,41 @@
 package donkers.tutorial.medium
 
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.test.context.junit.jupiter.SpringExtension
 
-@SpringBootTest
-class TutorialRestApiApplicationTests {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(SpringExtension::class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class TutorialRestApiApplicationTests @Autowired constructor(
+	private val restTemplate: TestRestTemplate
+){
+
+	@LocalServerPort
+	private var port: Int = 0
+
+	private fun getRootUrl(): String = "http://localhost:$port/status"
 
 	@Test
 	fun contextLoads() {
 	}
 
+	@Test
+	fun `status should return message Server Ok!`() {
+
+		val response = restTemplate.getForEntity(
+			getRootUrl(),
+			StatusObject::class.java
+		)
+
+		Assertions.assertEquals(200, response.statusCode.value())
+		Assertions.assertNotNull(response.body)
+		Assertions.assertEquals("Server Ok!", response.body?.message)
+	}
 }
